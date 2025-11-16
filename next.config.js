460c118d4192e919962c823c39cad59a90862ddb
khargacode <khargatraders@gmail.com>
2025-11-16 16:34:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  },
   images: {
-    domains: ["res.cloudinary.com"],
-    formats: ["image/avif", "image/webp"]
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
