bf4e253031072b12d2d42166e531e08efb482462
khargacode <khargatraders@gmail.com>
2025-11-16 18:02:57 +0530

    Create next-auth.d.ts

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface User {
+    id: string;
+  }
+
+  interface Session {
+    user: {
+      id: string;
+      email: string;
+      name?: string;
+    }
+  }
+}
